2ca7355d129d4ecd8097ae7874df43ecc4ed2cb9
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2020-12-31 10:02:14 +0100

    Added responsiveness

--- src/Components/HeroSection/Hero.js
+++ src/Components/HeroSection/Hero.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import './Herosection.css';
+import thankgod from './hero-image.png'
 
 const Hero = () => {
     return (
         <div className="Herosection__">
             <div className="introduction__">
+            <div className="image">
+            </div>
                 <h3 className="contentIntro__">
                     Eboreime ThankGod
                 </h3>
-                <h1 className="remark__"> AMAZING,</h1>
+                <h1 className="remark__"> AMAZING, <b className="react">React</b></h1>
                 <h1 className="developer__">FRONT END DEVELOPER</h1>
                 <button className="btn__">RESUME</button>
-            </div>
-            <div className="image">
-            
             </div>
         </div>
     )
